e950367aa9265622cbde71cbd6ec5df9655f7966
Josh Strawbridge <HolyKnightJoshua@gmail.com>
2020-10-27 16:46:45 -0700

    Syntax and Syntax Sugar Exercise

---
+++ SyntaxAndSugarExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SyntaxAndSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give us a number.");
+            var answer = Console.ReadLine();
+
+            var response = (int.Parse(answer) < 9) ? $"{answer} is less than nine." : $"{answer} is greater than or equal to nine.";
+
+            Console.WriteLine($"{response}");
+        }
+    }
+}
